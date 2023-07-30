34595a2b6b7f996774ab3fc3229783959e63bda8
Rakesh Joshi <113878318+rakeshjoshi369@users.noreply.github.com>
2023-07-30 16:07:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    console.log("skillsMember");
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+    var memberSkills = document.getElementById("memberSkills");
+    skills.style.display = "none";
+    member.style.display = "block";
+    skillsMember.style.display = "block";
+    memberSkills.style.display = "none";
+}
